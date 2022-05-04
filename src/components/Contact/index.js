f37f681ac62e8ea1de8230504290c63de7f1e8e6
Sonarie <sonarie1980@gmail.com>
2022-05-04 17:33:55 -0500

    contact form complete

---
+++ src/components/Contact/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable no-unused-vars */
+import react from "react";
+
+function ContactForm() {
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" />
+                </div>
+            </form>
+            <button type="submit">Submit</button>
+        </section>
+    )
+}
+
+export default ContactForm;
